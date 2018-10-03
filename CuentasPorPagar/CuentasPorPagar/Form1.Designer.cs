056771cfa4fc5302a52eec5f55e3ea93f53eb858
brian correa <brianrafael0313@hotmail.com>
2018-10-03 16:02:09 -0400

    mover el form

--- CuentasPorPagar/CuentasPorPagar/Form1.Designer.cs
+++ CuentasPorPagar/CuentasPorPagar/Form1.Designer.cs
@@ -143,6 +143,7 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(909, 21);
             this.panel2.TabIndex = 1;
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
             // 
             // panel3
             // 
--- CuentasPorPagar/CuentasPorPagar/Form1.cs
+++ CuentasPorPagar/CuentasPorPagar/Form1.cs
@@ -12,12 +12,26 @@ namespace CuentasPorPagar
 {
     public partial class Form1 : Form
     {
+        public int xClick = 0, yClick = 0;
         public Form1()
         {
             InitializeComponent();
             Selector.Height = btnPanelPrincipal.Height;
             Selector.Top = btnPanelPrincipal.Top;
             pnPrincipal1.BringToFront();
+        }
+
+        private void panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+            {
+                xClick = e.X; yClick = e.Y;
+            }
+            else
+            {
+                this.Left = this.Left + (e.X - xClick);
+                this.Top = this.Top + (e.Y - yClick);
+            }
         }
 
         private void btnPanelPrincipal_Click(object sender, EventArgs e)
@@ -32,7 +46,7 @@ namespace CuentasPorPagar
             Selector.Height = btnPanelMantenimientos.Height;
             Selector.Top = btnPanelMantenimientos.Top;
             pnMantenimientos1.BringToFront();
-        }
+        }        
 
         private void btnCerrar_Click(object sender, EventArgs e)
         {
